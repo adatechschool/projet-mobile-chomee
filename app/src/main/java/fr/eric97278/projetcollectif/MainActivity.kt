079965333a819543e8773d5eb91dfbe272b11e94
eric97278 <159042489+eric97278@users.noreply.github.com>
2024-09-02 12:35:35 +0200

    parse données json et affichage

--- app/src/main/java/fr/eric97278/projetcollectif/MainActivity.kt
+++ app/src/main/java/fr/eric97278/projetcollectif/MainActivity.kt
@@ -9,6 +9,13 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Spinner
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import fr.eric97278.projetcollectif.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -33,7 +40,47 @@ class MainActivity : AppCompatActivity() {
 //                .setAction("Action", null)
 //                .setAnchorView(R.id.fab).show()
 //        }
+        getData()
     }
+private fun getData() {
+    val titleView = findViewById<TextView>(R.id.first_element);
+    val descriptionView = findViewById<TextView>(R.id.description);
+    val attractionsView = findViewById<Spinner>(R.id.attractions);
+    val dishesView = findViewById<Spinner>(R.id.dishes);
+    val activitiesView = findViewById<Spinner>(R.id.activities);
+
+
+    // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://freetestapi.com/api/v1/destinations/1"
+
+// Request a string response from the provided URL.
+        val stringRequest = JsonObjectRequest(
+            Request.Method.GET, url,null,
+            { response ->
+                val name = response.getString("name")
+                val country = response.getString("country")
+                val image = response.getString("image")
+                val continent = response.getString("continent")
+                val description = response.getString("description")
+                val attractions = response.getJSONArray("attractions")
+                val dishes = response.getJSONArray("dishes")
+                val activities = response.getJSONArray("activities")
+
+                // Display the first 500 characters of the response string.
+                titleView.text = "$name, $country, $continent"
+                descriptionView.text = "$description"
+                attractionsView.li = "$attractions"
+                dishesView.text = "$dishes"
+                activitiesView.text = "$activities"
+
+
+            },
+            { titleView.text = "That didn't work!" })
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)
+}
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
